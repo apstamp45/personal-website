0204798bfd769cf95f42cb4bf93808ec49420631
apstamp45 <origamifreak55@gmail.com>
2023-03-17 22:34:23 -0400

    Finally making changes.

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 window.onload = function() {
-	//alert("hi");
 }
+
+document.getElementById("dropdown")
+		.addEventListener("mouseover", function() {
+	let items = document.getElementsByClassName("dropdown-item");
+	for (let i = 0; i < items.length; i++) {
+		items[i].classList.add("visible");
+	}
+});
+
+document.getElementById("dropdown")
+		.addEventListener("mouseout", function() {
+	let items = document.getElementsByClassName("dropdown-item");
+	for (let i = 0; i < items.length; i++) {
+		items[i].classList.remove("visible");
+	}
+});

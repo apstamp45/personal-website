32118511b75dc57074dd7e67abd6ab725b662b2a
apstamp45 <origamifreak55@gmail.com>
2023-06-07 19:00:43 -0400

    Blog home changed.

--- blog/script.js
+++ blog/script.js
@@ -1,12 +1,2 @@
 window.addEventListener("load", () => {
-	let posts = document.getElementsByClassName("post");
-	//let fileReader = new FileReader();
-	for (post of posts) {
-		let url = post.getAttribute("url");
-		let ajax = new XMLHttpRequest();
-		ajax.open("GET", url, false);
-		ajax.send();
-		let html = $.parseHTML(ajax.responseText);
-		post.innerHTML = html.getElementByID("date");
-	}
 });
